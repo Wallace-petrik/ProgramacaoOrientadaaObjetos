21f05232435af9e867a8d468a1859fa182b3bf3c
Wallace Petrik <wallacepetrik@gmail.com>
2024-04-08 14:00:37 -0300

    Projeto 2

---
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo3/Exemplos/Projeto2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero1, numero2, soma;
+        numero1 = 10;
+        numero2 = 15;
+        soma = numero1+numero2;
+        System.out.println(numero1+" + "+numero2+" = "+soma);
+    }
+}
