d4460a6f949fb1a4cad12bcb6d04d55949dd48b1
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2024-10-24 14:59:01 -0300

    Capítulo 4: Implementação de Classes
     Exercício 12, implementação da classe Ponto

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo4/ExerciciosPropostos/Exercicios4.15/Exercicio12/src/Ponto.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo4/ExerciciosPropostos/Exercicios4.15/Exercicio12/src/Ponto.java
@@ -7,25 +7,20 @@ public class Ponto {
         this.x = 0.0;
         this.y = 0.0;
     }
-
     public Ponto(double valorX, double valorY){
         this.x = valorX;
         this.y = valorY;
     }
-
     public double informarAbcissa(){
         return x;
     }
-
     public  double informarOrdenada(){
         return y;
     }
-
     public void redefina_se(double valorX, double valorY){
         x = valorX;
         y = valorY;
     }
-
     public double fornecaDistanciaAte (Ponto outroPonto){
         double outroX, outroY, distancia;
         outroX = outroPonto.informarAbcissa();
@@ -33,5 +28,4 @@ public class Ponto {
         distancia = Math.sqrt((x-outroX)*(x-outroX) + (y-outroY)*y+outroY);
         return  distancia;
     }
-
 }
