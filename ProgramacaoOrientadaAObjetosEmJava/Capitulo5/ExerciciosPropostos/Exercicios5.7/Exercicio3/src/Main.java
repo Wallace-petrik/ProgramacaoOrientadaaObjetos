897981c158922df72931f03ad3661d08660513eb
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-03-06 21:46:08 -0300

    Reescrever o do-while, for e while

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio3/src/Main.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio3/src/Main.java
@@ -1,7 +1,32 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        
 
+
+        int cont = 0;
+        int soma = 0;
+
+        do {
+
+            soma += 1;
+
+        }while (cont <= 10);
+
+        cont = 0;
+        soma = 0;
+
+        for (cont = 0; cont <= 10; cont++){
+            soma += 1;
+        }
+
+        cont = 0;
+        soma = 0;
+
+        while (cont<=10){
+            soma += 1;
+            cont++;
+        }
     }
 }
