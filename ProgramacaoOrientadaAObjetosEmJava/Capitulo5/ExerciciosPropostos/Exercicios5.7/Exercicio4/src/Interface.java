219c23f33b0e1a5392e5c85420fc0baede132756
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-03-10 19:18:01 -0300

    Implementação do método para coleta de dados

---
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio4/src/Interface.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Interface {
+
+    public Pessoa peguePessoa (){
+        String nome = JOptionPane.showInputDialog(null,"Digite o nome","nome",JOptionPane.INFORMATION_MESSAGE);
+        String idade = JOptionPane.showInputDialog(null,"Digite a idade","Idade",JOptionPane.INFORMATION_MESSAGE);
+        String sexo = JOptionPane.showInputDialog(null,"Digite o sexo","Sexo",JOptionPane.INFORMATION_MESSAGE);
+
+        int vIdade = Integer.parseInt(idade);
+        char vSexo = sexo.charAt(0);
+
+        return new Pessoa();
+    }
+
+
+}
