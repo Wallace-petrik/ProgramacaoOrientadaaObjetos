5ded943cccb83be9b8fcf240951393f5222ed9cf
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-03-10 18:22:58 -0300

    Métodos contrutores

---
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio4/src/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+
+    protected String nome;
+    protected int idade;
+    protected char sexo;
+
+    public Pessoa(){
+    }
+    public Pessoa(String vMome,char vSexo,int idade){
+        this.nome = vMome;
+        this.sexo = vSexo;
+        this.idade = idade;
+    }
+}
