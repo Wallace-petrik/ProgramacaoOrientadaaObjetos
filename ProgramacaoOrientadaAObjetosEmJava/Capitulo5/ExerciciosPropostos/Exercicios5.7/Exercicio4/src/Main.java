3682ad049f73f78b21cef6af81198e60bcadcc66
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-03-12 21:15:02 -0300

    Percentual de Homens

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio4/src/Main.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio4/src/Main.java
@@ -4,6 +4,28 @@ public class Main {
         Interface umaInterface = new Interface();
         Pessoa umaPessoa;
 
+        double soma = 0,percentualH = 0;
+        int cont = 0;
+        int contM = 0;
+        char resp;
 
+        do{
+
+            umaPessoa = umaInterface.peguePessoa();
+            soma += umaPessoa.getIdade();
+            if(umaPessoa.sexo=='m'||umaPessoa.sexo == 'M'){
+                if(umaPessoa.idade >= 70){
+                    contM++;
+                }
+            }
+            cont++;
+            resp = umaInterface.temMais();
+        }while (resp == 's' || resp == 'S');
+
+        percentualH = ((double) contM * 100) / cont;
+
+        double meida = soma/cont;
+        umaInterface.mostrarMedia(meida);
+        System.out.println("Percentual de homens com mais de 70: "+percentualH);
     }
 }
