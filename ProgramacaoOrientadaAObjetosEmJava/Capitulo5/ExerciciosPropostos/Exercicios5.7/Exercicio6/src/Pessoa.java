357e2f2e83e0df02dae35a2870474ceee2a1936d
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-03-18 18:40:35 -0300

    Método para calcular peso ideal

---
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio6/src/Pessoa.java
@@ -0,0 +1,53 @@
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected char sexo;
+
+    public Pessoa(){
+    }
+    public Pessoa(String vNome, char vSexo, int vIdade){
+        this.nome = vNome;
+        this.sexo = vSexo;
+        this.idade = vIdade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void facaAiniversario(){
+        this.idade++;
+    }
+
+    public float pesoIdeal(float vAltura){
+        float peso = 0;
+
+        if(sexo=='m'||sexo=='M'){
+            peso = 72.7f * vAltura - 58;
+        }else{
+            peso = 62.1f * vAltura - 44.7f;
+        }
+        return peso;
+    }
+
+}
