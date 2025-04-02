55f63e552a2871ce6c670a626b59763f8b2f3f45
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-04-02 07:13:01 -0300

    Métodos Acessores Implementação dos métodos get e set da parte real e imaginária do número complexo.

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio14/src/NumeroComplexo.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio14/src/NumeroComplexo.java
@@ -1,5 +1,29 @@
 public class NumeroComplexo {
 
+    /*
+    Um número complexo é dado por Z = a+ bi e é identificado com o
+    ponto do plano que tem coordenadas (a,b). O número complexo tem
+    uma  parte real a e uma parte imaginária b  em que a parte imaginária
+    corresponde à raiz de um número negativo.. Se Z = a temos um
+    número real e se Z = bi temos um número imaginário.
+    */
 
+    private double parteReal;
+    private double parteImaginaria;
 
+    public double getParteImaginaria() {
+        return parteImaginaria;
+    }
+
+    public double getParteReal() {
+        return parteReal;
+    }
+
+    public void setParteImaginaria(double parteImaginaria) {
+        this.parteImaginaria = parteImaginaria;
+    }
+
+    public void setParteReal(double parteReal) {
+        this.parteReal = parteReal;
+    }
 }
