59fe5938f6020a30cf027938f2319f2517a33d25
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-04-21 06:58:03 -0300

    Implementação dos métodos construtores

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio15/src/ContaBancaria.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio15/src/ContaBancaria.java
@@ -1,3 +1,20 @@
 public class ContaBancaria {
 
+    private String nome;
+    private int id;
+    private double saldo;
+    private double limiteSaldoNegativo;
+
+    public ContaBancaria(String vNome, int vId, double vSaldo, double vLimiteSaldoNegativo){
+        this.nome = vNome;
+        this.id = vId;
+        this.saldo = vSaldo;
+        this.limiteSaldoNegativo = vLimiteSaldoNegativo;
+    }
+
+    public ContaBancaria(int vId, String vNome){
+        this.id = vId;
+        this.nome = vNome;
+    }
+
 }
