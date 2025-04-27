f8471e927a0b0a78305ad1e7ef9310efac1f9e87
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-04-27 08:07:36 -0300

    Update chequeEspecial

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio15/src/ContaBancaria.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio15/src/ContaBancaria.java
@@ -5,13 +5,13 @@ public class ContaBancaria {
     private String nome;
     private int id;
     private double saldo;
-    private double limiteSaldoNegativo;
+    private double chequeEspecial;
 
     public ContaBancaria(String vNome, int vId, double vSaldo){
         this.nome = vNome;
         this.id = vId;
         this.saldo = vSaldo;
-        this.limiteSaldoNegativo = 50;
+        this.chequeEspecial = 50;
     }
 
     public ContaBancaria(int vId, String vNome){
@@ -31,8 +31,8 @@ public class ContaBancaria {
         return saldo;
     }
 
-    public double getLimiteSaldoNegativo() {
-        return limiteSaldoNegativo;
+    public double getchequeEspecial() {
+        return chequeEspecial;
     }
 
     public void setNome(String nome) {
@@ -47,8 +47,8 @@ public class ContaBancaria {
         this.saldo = saldo;
     }
 
-    public void setLimiteSaldoNegativo(double limiteSaldoNegativo) {
-        this.limiteSaldoNegativo = limiteSaldoNegativo;
+    public void setchequeEspecial(double chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
     }
 
     private float entradaDeDados(String tipoDeEntrada){
@@ -56,6 +56,22 @@ public class ContaBancaria {
         Scanner teclado = new Scanner(System.in);
         valor = teclado.nextFloat();
         return  valor;
+    }
+
+    private float chequeEspecial(float valores, int tipoDeOperacao){
+        // 1 para deposito e 2 para saque.
+        if(tipoDeOperacao==1){
+            if(valores>=50){
+                if(chequeEspecial!=50){
+                    chequeEspecial += 50-chequeEspecial;
+                }
+            }else{
+
+            }
+        }else if (tipoDeOperacao==2){
+
+        }
+        return 0;
     }
 
     public void deposito(float valorDeposito){
