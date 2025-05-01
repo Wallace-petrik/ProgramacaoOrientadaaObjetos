be4a27cefe20ed8ed81b13539b7f495414a532dc
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-05-01 06:45:10 -0300

    Implementação dos métodos Get

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio16/src/Ponto.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio16/src/Ponto.java
@@ -6,9 +6,19 @@ public class Ponto {
         this.x = 0;
         this.y = 0;
     }
+
     public Ponto(double vX, double vY){
         this.x = vX;
         this.y = vY;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
 
 }
