1accc2bf11d98c64fc3256a51d9ed3ba5cbb3921
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-05-01 06:36:03 -0300

    Implementação do métodos construdores Classe Ponto

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio16/src/Ponto.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio16/src/Ponto.java
@@ -1,2 +1,14 @@
 public class Ponto {
+    protected double x;
+    protected double y;
+
+    public Ponto (){
+        this.x = 0;
+        this.y = 0;
+    }
+    public Ponto(double vX, double vY){
+        this.x = vX;
+        this.y = vY;
+    }
+
 }
