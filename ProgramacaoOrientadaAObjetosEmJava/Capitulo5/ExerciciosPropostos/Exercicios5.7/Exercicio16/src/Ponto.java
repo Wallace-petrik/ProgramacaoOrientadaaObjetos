550d8a0a6a6371b09334db354eae114afa94fdca
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-05-02 06:42:37 -0300

    Implementação do método quadante O método fornece em que quadrante está o ponto

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio16/src/Ponto.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio16/src/Ponto.java
@@ -32,4 +32,17 @@ public class Ponto {
         distancia = Math.sqrt((x-outroX)*(x-outroX) + (y-outroY)*(y-outroY));
         return distancia;
     }
+
+    public int quadrante(){
+        if(x > 0 && y >0 ){
+            return 1;
+        } else if (y > 0 && x < 0) {
+            return 2;
+        } else if (x < 0 && y < 0) {
+            return 3;
+        }else if (y < 0 && x > 0){
+            return 4;
+        }
+        return 0;
+    }
 }
