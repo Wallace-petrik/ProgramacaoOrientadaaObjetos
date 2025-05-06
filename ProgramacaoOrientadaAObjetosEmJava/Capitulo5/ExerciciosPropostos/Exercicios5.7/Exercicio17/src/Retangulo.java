9c67057eacec73a66d19c1ea25a6c77910af0c76
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-05-06 17:54:04 -0300

    Menu de Opção

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio17/src/Retangulo.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio17/src/Retangulo.java
@@ -1,2 +1,18 @@
 public class Retangulo {
+
+    public Retangulo(){
+
+    }
+
+    int opcao;
+
+    void menuDeOpcao(int vOpcao){
+        System.out.println("Entrar com os dados de um novo retângulo");
+        switch (opcao){
+            case 1:{
+
+                break;
+            }
+        }
+    }
 }
