ffd1d13aa16653133a0e1b86c4955bcc4fbdf106
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-05-07 21:56:11 -0300

    Update Retangulo

--- ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio17/src/Retangulo.java
+++ ProgramacaoOrientadaAObjetosEmJava/Capitulo5/ExerciciosPropostos/Exercicios5.7/Exercicio17/src/Retangulo.java
@@ -4,15 +4,4 @@ public class Retangulo {
 
     }
 
-    int opcao;
-
-    void menuDeOpcao(int vOpcao){
-        System.out.println("Entrar com os dados de um novo retângulo");
-        switch (opcao){
-            case 1:{
-
-                break;
-            }
-        }
-    }
 }
